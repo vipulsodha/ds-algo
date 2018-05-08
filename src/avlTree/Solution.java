9c3abf56ec1a0ba3c9f21a72007a18f3af1c63de
Vipul Sodha <sodha_v@worksap.co.jp>
2018-05-08 19:15:50 +0800

    avl

--- src/avlTree/Solution.java
+++ src/avlTree/Solution.java
@@ -1,8 +1,7 @@
 package avlTree;
 
 
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 /**
  * @author sodha
@@ -20,10 +19,6 @@ public class Solution {
     }
 
 
-    static void buildAvl(Node node) {
-
-    }
-
     static void inOrder(Node node) {
 
         if (!Objects.isNull(node)) {
@@ -36,15 +31,11 @@ public class Solution {
 
     }
 
-
-
-
     static void add(int key) {
 
+//            root = insert(root, key);
 
-            root = insert(root, key);
-
-
+        root = nonBalanceInsert(root, key);
     }
 
 
@@ -65,7 +56,6 @@ public class Solution {
 
         Node left = node.left;
 
-
         node.right = right.left;
 
         right.left = node;
@@ -76,22 +66,18 @@ public class Solution {
 
         return  right;
 
-
     }
 
 
     static Node rightRotate(Node node) {
 
-
         Node right = node.right;
 
         Node left = node.left;
 
-
         node.left = left.right;
 
         left.right = node;
-
 
         node.ht = Math.max(ht(node.left), ht(node.right)) + 1;
 
@@ -99,6 +85,20 @@ public class Solution {
 
         return left;
 
+    }
+
+    static Node nonBalanceInsert(Node node, int key) {
+        if (node == null) {
+            return  createNodeFromKey(key);
+        }
+
+        if (node.key >= key) {
+            node.left = nonBalanceInsert(node.left, key);
+        } else {
+            node.right = nonBalanceInsert(node.right, key);
+        }
+
+        return node;
     }
 
 
@@ -114,22 +114,17 @@ public class Solution {
             node.right = insert(node.right, key);
         }
 
-
         node.ht = Math.max(ht(node.left), ht(node.right)) + 1;
 
-
         int balance = ht(node.right) - ht(node.left);
-
 
         // right right
         if (balance > 1 && node.right != null && key > node.right.key) {
 
-
-//            leftrotate
             return  leftRotate(node);
 
-
         }
+
 
         // right left
         if (balance > 1 && node.left != null && key <  node.left.key) {
@@ -137,17 +132,10 @@ public class Solution {
 
             node.right = rightRotate(node.right);
             return leftRotate(node);
-
-            // rightRotate(node->right)
-            // left rotate(node)
-
         }
 
         // left left
         if (balance < -1 && node.left != null && key <  node.left.key) {
-
-
-            // right rotate
 
             return rightRotate(node);
 
@@ -156,10 +144,6 @@ public class Solution {
 
         // left right
         if (balance < -1 && node.right != null && key <  node.right.key) {
-
-
-            // leftRotate(node->left)
-            // rightRotate(node)
 
             node.left = leftRotate(node.left);
 
@@ -179,9 +163,219 @@ public class Solution {
         add(15);
         add(20);
         add(25);
-        System.out.printf("");
+//        System.out.printf("");
+
+//        levelOrderTraversal(root);
+
+//        verticalTraversal(root);
+
+//        topView(root);
+
+        customtopView(root);
+    }
 
 
+
+
+    static void customtopView(Node root) {
+        TreeMap<Integer, List<Node>> map = new TreeMap<Integer, List<Node>>();
+
+        Map<Node, Integer> hdMap = new HashMap<Node, Integer>();
+
+        hdMap.put(root, 0);
+
+        Queue<Node> queue = new LinkedList<Node>();
+        map.put(0, new ArrayList<Node>());
+        map.get(0).add(root);
+
+        queue.add(root);
+
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.poll();
+
+            Node left = node.left;
+
+            Node right = node.right;
+
+            if (left != null) {
+
+                int hd = hdMap.get(node) - 1;
+                hdMap.put(left, hd);
+
+                if (!map.containsKey(hd)) {
+                    map.put(hd, new ArrayList<Node>());
+                }
+
+                map.get(hd).add(left);
+                queue.add(left);
+            }
+
+            if (right != null) {
+
+                int hd = hdMap.get(node) + 1;
+                hdMap.put(right, hd);
+
+                if (!map.containsKey(hd)) {
+                    map.put(hd, new ArrayList<Node>());
+                }
+
+                map.get(hd).add(right);
+                queue.add(right);
+            }
+
+        }
+
+        for (Map.Entry<Integer, List<Node>> e: map.entrySet()) {
+
+            System.out.print(e.getValue().get(0).key + " ");
+        }
+
+    }
+
+
+
+    static void levelOrderTraversal(Node root) {
+
+
+        Queue<Node> queue = new LinkedList<>();
+
+        root.hd = 0;
+
+
+        queue.add(root);
+
+
+        while (!queue.isEmpty()) {
+
+
+            Node node = queue.poll();
+
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+
+            System.out.print(node.key + " ");
+
+        }
+
+
+    }
+
+    static void topView(Node root) {
+        TreeMap<Integer, List<Node>> map = new TreeMap<>();
+
+
+        root.hd = 0;
+
+        Queue<Node> queue = new LinkedList<>();
+        map.put(root.hd, new ArrayList<>());
+        map.get(root.hd).add(root);
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.poll();
+
+            Node left = node.left;
+
+            Node right = node.right;
+
+            if (left != null) {
+                left.hd = node.hd -1;
+
+                if (!map.containsKey(left.hd)) {
+                    map.put(left.hd, new ArrayList<>());
+                }
+
+                map.get(left.hd).add(left);
+                queue.add(left);
+            }
+
+            if (right != null) {
+                right.hd = node.hd + 1;
+
+                if (!map.containsKey(right.hd)) {
+                    map.put(right.hd, new ArrayList<>());
+                }
+
+                map.get(right.hd).add(right);
+                queue.add(right);
+            }
+
+        }
+
+        for (Map.Entry<Integer, List<Node>> e: map.entrySet()) {
+
+
+            System.out.println(e.getValue().get(0).key);
+
+            System.out.println("");
+        }
+    }
+
+
+    static void verticalTraversal(Node root) {
+
+        TreeMap<Integer, List<Node>> map = new TreeMap<>();
+
+
+        root.hd = 0;
+
+        Queue<Node> queue = new LinkedList<>();
+        map.put(root.hd, new ArrayList<>());
+        map.get(root.hd).add(root);
+
+
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+
+            Node node = queue.poll();
+
+            Node left = node.left;
+
+            Node right = node.right;
+
+            if (left != null) {
+                left.hd = node.hd -1;
+
+                if (!map.containsKey(left.hd)) {
+                    map.put(left.hd, new ArrayList<>());
+                }
+
+                map.get(left.hd).add(left);
+                queue.add(left);
+            }
+
+            if (right != null) {
+                right.hd = node.hd + 1;
+
+                if (!map.containsKey(right.hd)) {
+                    map.put(right.hd, new ArrayList<>());
+                }
+
+                map.get(right.hd).add(right);
+                queue.add(right);
+            }
+
+        }
+
+        for (Map.Entry<Integer, List<Node>> e: map.entrySet()) {
+
+
+            for (Node i : e.getValue()) {
+                System.out.print(i.key + " ");
+            }
+
+            System.out.println("");
+        }
     }
 
 }
@@ -203,9 +397,11 @@ class Node {
 
     Node left;
 
-    int ht = 0;
+    int ht = 1;
 
     int key;
+
+    int hd;
 
 
 }
